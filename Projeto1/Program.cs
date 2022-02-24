7dcb3ce98e6010121cbd9aebf0ce817cb137d256
Micael Teixeira <a22103816@alunos.ulht.pt>
2022-02-24 09:12:52 +0000

    Add bye world

--- Projeto1/Program.cs
+++ Projeto1/Program.cs
@@ -7,6 +7,7 @@ namespace Projeto1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bye World!");
         }
     }
 }
