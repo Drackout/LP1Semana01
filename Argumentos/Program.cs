59212b761410e111920d3b2ef695ac33e99723fc
Micael Teixeira <a22103816@alunos.ulht.pt>
2022-02-24 09:32:30 +0000

    Add Argumentos

---
+++ Argumentos/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Argumentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Argumentos!");
+            for(int i=0; i<args.Length(); i++ )
+            {
+                Console.WriteLine("Argumento "+i+" -> " +args[i]);
+            }
+        }
+    }
+}
