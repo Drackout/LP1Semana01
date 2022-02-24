55126618913d3074be0e9ebe37cfdb4f1794d43f
Micael Teixeira <a22103816@alunos.ulht.pt>
2022-02-24 15:55:07 +0200

    Varios Chars

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -6,11 +6,20 @@ namespace TiposVarios
     {
         static void Main(string[] args)
         {
+            //inteiros
             int intSimple= 9;
             uint uIntSimple = 8;
-            
             Console.WriteLine("Int -> " + intSimple);
             Console.WriteLine("UInt -> " + uIntSimple);
+
+            //chars
+            char charSimple = '\u00A9';
+            char charSimpleTwo = '\\';
+            char charSimpleThree = '\u1234';
+            Console.WriteLine("Char -> " + charSimple);
+            Console.WriteLine("CharTwo -> " + charSimpleTwo);
+            Console.WriteLine("CharThree -> " + charSimpleThree);
+
         }
     }
 }
