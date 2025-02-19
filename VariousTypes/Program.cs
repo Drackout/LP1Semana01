45f6185fc0d2c50b0f0e8a5b2fdd0a1e80692159
Drackout <mikatx2@hotmail.com>
2025-02-19 21:02:22 +0000

    Add chars

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -9,12 +9,16 @@ namespace VariousTypes
             int inteiro1 = 11;
             uint inteiro2 = 21U;
             int inteiro30 = 0x10;
-
             Console.WriteLine(inteiro1);
             Console.WriteLine(inteiro2);
             Console.WriteLine(inteiro30);
 
-
+            char unic1 = '\u001A';
+            char unic2 = '\u0A29';
+            char unic3 = '\u2609';
+            Console.WriteLine(unic1);
+            Console.WriteLine(unic2);
+            Console.WriteLine(unic3);
         }
     }
 }
