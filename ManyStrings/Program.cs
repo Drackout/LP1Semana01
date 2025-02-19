9065e08504a96314958ffe854741ee48faddb892
Drackout <mikatx2@hotmail.com>
2025-02-19 22:35:40 +0000

    testes com strings, concatenações e interpolações

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -9,16 +9,27 @@ namespace ManyStrings
         {
             Console.OutputEncoding = Encoding.UTF8;
 
+            int x=1, y=2;
             string str1;
             str1 = "isto é uma string";
 
             string str2 = "\u0a02 <- ayy lmao, ayy limão -> \uF0F2";
 
             string str3 = "tabs!\tTabs!";
-            
             Console.WriteLine(str1);
             Console.WriteLine(str2);
             Console.WriteLine(str3);
+
+            string str1Concat = "string concat ->'"+str1+"'<-";
+            string str2Concat = 2+" "+"s"+"a"+"p"+"o"+"s";
+            Console.WriteLine(str1Concat);
+            Console.WriteLine(str2Concat);
+            
+            string str1Interp = $"quantos sapos?: {str2Concat}";
+            string str2Interp = $"soma de {x} + {y}: {y+x}";
+            Console.WriteLine(str1Interp);
+            Console.WriteLine(str2Interp);
+
         }
     }
 }
