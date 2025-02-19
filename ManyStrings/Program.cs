e490dfb0e677233605374891a9695811971fd55f
Drackout <mikatx2@hotmail.com>
2025-02-19 22:28:42 +0000

    Create and modify program.cs

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace ManyStrings
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            string str1;
+            str1 = "isto é uma string";
+
+            string str2 = "\u0a02 <- ayy lmao, ayy limão -> \uF0F2";
+
+            string str3 = "tabs!\tTabs!";
+            
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+            Console.WriteLine(str3);
+        }
+    }
+}
