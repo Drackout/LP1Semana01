dfc307ab7d29e3b71eb1d4e11090a1f0c41e6b52
Drackout <mikatx2@hotmail.com>
2025-02-19 22:58:09 +0000

    General idea of the program

---
+++ AskForInput/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AskForInput
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string userTxt;
+            int intCon;
+            float floatConverted;
+
+            userTxt = Console.ReadLine();
+            intConverted = Convert.ToInt16(userTxt);
+
+            userTxt = Console.ReadLine();
+            floatConverted = float.Parse(userTxt);
+
+            Console.WriteLine($"Sum of {intConverted} + {floatConverted} = {intConverted + floatConverted}");
+
+
+        }
+    }
+}
